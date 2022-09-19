05fedbcd0c268eadfaa339de05103b29a21268a9
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-19 16:23:14 -0300

    Add book list saver script

--- scripts.js
+++ scripts.js
@@ -1,15 +1,39 @@
-const arr = [
-  {
-    title: 'Example Book',
-    author: 'John Doe',
-  },
-];
-
 const booklist = document.querySelector('#bookcontainer');
-arr.forEach((book) => {
-  booklist.innerHTML = `<div class="book">
-  <p class="title">${book.title}</p>
-  <p class="author">${book.author}</p>
-  <button class="remover">Remove</button>
-</div>`;
+
+const form = document.querySelector('#form');
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function record() {
+  let arr = JSON.parse(window.localStorage.getItem('bookarr'));
+  console.info(arr);
+  if (arr === null) {
+    arr = [];
+  }
+  const title = form.querySelector('#title');
+  const author = form.querySelector('#author');
+  const titlename = title.value;
+  const authorname = author.value;
+  const newbook = new Book(titlename, authorname);
+  arr.push(newbook);
+  window.localStorage.setItem('bookarr', JSON.stringify(arr));
+}
+
+window.addEventListener('load', () => {
+  const arr = JSON.parse(window.localStorage.getItem('bookarr'));
+  console.log(arr);
+  arr.forEach((item) => {
+    booklist.innerHTML = `<div class="book">
+    <p class="title">${item.title}</p>
+    <p class="author">${item.author}</p>
+    <button class="remover">Remove</button>
+  </div>`;
+  });
+});
+
+form.addEventListener('submit', () => {
+  record();
 });
